e6533830983344eb1d73a34407597dd41255a896
Sharma Elanthiriayan <sharma.elanthiraiyan@nagarro.com>
2021-06-13 17:42:00 +0530

    Initial commit

---
+++ WWDC-2021/WWDC_2021App.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC_2021App.swift
+//  WWDC-2021
+//
+//  Created by Sharma Elanthiraiyan on 13/06/21.
+//
+
+import SwiftUI
+
+@main
+struct WWDC_2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
